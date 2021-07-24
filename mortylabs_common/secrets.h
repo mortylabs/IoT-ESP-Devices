1cd29a2dfe0d4f7f98dcabd655462e153f44874c
Andrew Morty <mortyone@hotmail.com>
2021-07-24 11:37:48 +0100

    initial creation

---
+++ mortylabs_common/secrets.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID "REPLACE_ME"
+#define WIFI_PASS "REPLACE_ME"
+
+#define MQTT_SERVER "192.168.1.15"
+#define MQTT_PORT   1883
+#define MQTT_USER   "REPLACE_ME"
+#define MQTT_PASS   "REPLACE_ME"
+
+
+
