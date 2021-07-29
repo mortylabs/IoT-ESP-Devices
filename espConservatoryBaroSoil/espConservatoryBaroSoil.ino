1afd5b957d77112e1defc95d40d01de71baa1011
Andrew Morty <mortyone@hotmail.com>
2021-07-29 12:34:32 +0100

    initial create

--- espConservatoryBaroSoil/espConservatoryBaroSoil.ino
+++ espConservatoryBaroSoil/espConservatoryBaroSoil.ino
@@ -2,184 +2,111 @@
 //SDA to D2
 //Board = NodeMCU 1.0  (ESP-12E Module)
 
-#include <mortylabs_esp8266_common.h>
-#include <secrets.h>
-#include <ArduinoOTA.h>
-#include <ESP8266WebServer.h>
+#include <espWebServerOTAMqtt.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <PubSubClient.h>
 
-char*   hostname      = "espConservatory1";  // give your device a unique name here
-float   LOCALALTUD    = 35.0;                // set your local altitude in meters
-float   deep_sleep_ms = 000e6;               // 0e6 = 0 secs (sleep disabled), 300e6 = 5 mins sleep, 600e6 = 10 mins sleep etc
-
-#define mqtt_t_heartbeat              "plants/conservatory1/LWT"
-
-#define mqtt_t_config_temp            "homeassistant/sensor/conservatory1_sensor_temp/config"
-#define mqtt_t_state_temp             "plants/conservatory1/temp/state"
-
-#define mqtt_t_config_baro            "homeassistant/sensor/conservatory1_sensor_baro/config"
-#define mqtt_t_state_baro             "plants/conservatory1/baro/state"
-
-#define mqtt_t_config_soil1           "homeassistant/sensor/conservatory1_sensor_soil1/config"
-#define mqtt_t_state_soil1            "plants/conservatory1/soil1/state"
-
-
-
-topicArray_t mqtt_topics[] = {
-//{"unique_id", "friendly name", "unit_of_measurement", "topic_config", "topic_state"}
-
-  {"conservatory_temperature", "Conservatory Temperature", "°C",  mqtt_t_config_temp,  mqtt_t_state_temp},
-  {"conservatory_baro",        "Conservatory Baro",        "hPa", mqtt_t_config_baro,  mqtt_t_state_baro},
-  {"conservatory_soil1",       "Conservatory Soil 1",       "%",  mqtt_t_config_soil1, mqtt_t_state_soil1}
-};
-
-
+MyCommon  client;
 Adafruit_BMP280 bmp;
 
 unsigned long   ms_mqtt_lastsent     = 0;
-unsigned long   ms_sensors_lastread  = 0;
-unsigned long   ms_startup           = millis();
 
+// in this sketch, the soil moisture sensor is connected to anolog A0
 int   analog_value         = 0;
-int   analog_value_max      = 780; // every soil sensor gives different max/min readings, do some tests and adjust here
-int   analog_value_min      = 250;  
-float soil_moisture       = 0;
-float baro_temperature    = 0;
-float pressure_raw        = 0;
-float pressure_ICAO       = 0;
-float pressure_bosch      = 0;
-float pressure_babinet    = 0;
+int   analog_soil_max     = 720; // every soil sensor gives different max/min readings, do some tests and adjust here
+int   analog_soil_min     = 250;  
+float soil_perc            = 0;
 
+//BMP280 barometer, there are a few "official" formulas calculating air pressure slightly differently
+float baro_temperature     = 0;
+float pressure_raw         = 0;
+float pressure_ICAO        = 0;
+float pressure_bosch       = 0;
+float pressure_babinet     = 0;
 
-ESP8266WebServer server(80);
-WiFiClient       espClient;
-PubSubClient     mqttClient(espClient); 
+#define mqtt_t_avail                  "plants/conservatory1/LWT"
+#define mqtt_t_temp_config            "homeassistant/sensor/conservatory1_sensor_temp/config"
+#define mqtt_t_temp_state             "plants/conservatory1/temp/state"
+#define mqtt_t_baro_config            "homeassistant/sensor/conservatory1_sensor_baro/config"
+#define mqtt_t_baro_state             "plants/conservatory1/baro/state"
+#define mqtt_t_soil1_config           "homeassistant/sensor/conservatory1_sensor_soil1/config"
+#define mqtt_t_soil1_state            "plants/conservatory1/soil1/state"
+#define LOCALALTUD      35.0      // [m] Set your local altitude in meters
+  
 
-MyCommon         myCommon(/*hostname*/ hostname,/*mqttClient*/ mqttClient, /*mqtt_topic_heartbeat*/ mqtt_t_heartbeat, /*mqtt_topics*/ mqtt_topics, /*debug*/ true, /*battery*/ false, deep_sleep_ms);
+void onConnectionEstablished() {
+  Serial.println("onConnectionEstablished");  
+}
 
+void set_html_index_body() {
+  String s = "<table><tr><th style='text-align:left;background-color:green;color:white;'>sensor</th><th style='text-align:left;background-color:green;color:white;'s>value</th></tr>";
+  
+  s= s + client.generate_html_table_row ("td","temperature", String(baro_temperature)+ " C");
+  s= s + client.generate_html_table_row ("td","pressure raw", String(pressure_raw / 100.0));
+  s= s + client.generate_html_table_row ("td","indoors (baro_temperature)", String(( pressure_raw  / pow(1.0 - 0.0065 * LOCALALTUD / (baro_temperature + 273.15), 5.255) / 100.0))); // ICAO formula
+  s= s + client.generate_html_table_row ("td","outdoors (ICAO 15C)", String(pressure_ICAO)); 
+  s= s + client.generate_html_table_row ("td","outdoors (BOSCH)", String(pressure_bosch)); 
+  s= s + client.generate_html_table_row ("td","outdoors (babinet)", String(pressure_babinet)); 
+  String a = "";
+  a = a + analog_value;
+  a = a + "   (  ";
+  a = a + soil_perc;
+  a = a + "% )  min=";
+  a = a + analog_soil_min;
+  a = a + ", max-=";
+  a = a + analog_soil_max;
+  s = s + client.generate_html_table_row ("td", "analog value (soil)", String(a)); 
+  s =s + "</table>";
 
+  client.set_html_index_body(s  );
+}
 
-void setup()
-{
-    Serial.begin(115200);
-    delay(100);
-    while (!Serial) { delay(1); }
+void setup() {
+  client.set_hostname("espConservatory1");
+  client.setup();
 
     if (!bmp.begin((0x76))) {
     //if (!bmp.begin()) { 
         Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-        while (1);
+      //  while (1);
     }
-
-    myCommon.begin();
-    
-    server.on("/", handleRoot);  
-    server.on("/reboot", []() { server.send(200, "text/plain", "Rebooting..."); delay(1000); ESP.restart();});
-    server.on("/wifi",   []() { server.send(200, "text/plain", myCommon.getWifiNetworkList()); });
-    server.on("/test",   []() { server.send(200, "text/plain", "Other URL"); });
-    server.onNotFound(   []() { server.send(404, "text/plain", "Ungaas baba, I 404 you..."); });
-    server.begin();  
-
-    mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
-    //mqttClient.setCallback(callback);   //no need for callback as we are only transmitting data, not awaiting commands
-    if (!mqttClient.connected()) { myCommon.mqtt_reconnect();  }
 }
 
-void read_sensors(){
+
+void read_sensors() {
+  Serial.println("READ SENSORS!");
   baro_temperature = bmp.readTemperature() ;
   pressure_raw     = bmp.readPressure(); 
   pressure_ICAO    = (pressure_raw  / pow(1.0 - 0.0065 * LOCALALTUD / (baro_temperature + 273.15), 5.255)) /100.00;
   pressure_bosch   = ( ((pressure_raw)/pow((1-((float)(LOCALALTUD))/44330), 5.255))/100.0); //BOSCH formula , std temp of 15C and good for indoors
   pressure_babinet = (- pressure_raw * (LOCALALTUD + 16000.0 + 64.0 * baro_temperature) / (LOCALALTUD - 16000.0 - 64.0 * baro_temperature))/100.0;
 
-  analog_value = 0;
   analog_value = analogRead(A0);
-  if (analog_value > analog_value_max) {
-      analog_value_max = analog_value;
-  }
-  if (analog_value < analog_value_min) {
-      analog_value_min = analog_value;
-  }
-  soil_moisture =  map(analog_value,analog_value_max,analog_value_min,0,100);
-  if (soil_moisture < 0) {
-      soil_moisture = 0;
-  }
-  ms_sensors_lastread = millis();
+  if (analog_value > analog_soil_max) { analog_soil_max = analog_value; }
+  if (analog_value < analog_soil_min and analog_value != 0) { analog_soil_min = analog_value; }
+  
+  soil_perc =  map(analog_value,analog_soil_max,analog_soil_min,0,100);
+  if (soil_perc <= 0 or analog_value == 0) { soil_perc = 0; }
+  set_html_index_body();     
   
 }
 
+
 void loop() {
-   if (WiFi.status() != WL_CONNECTED) { myCommon.connect_wifi(); }
-   server.handleClient();                    
-   ArduinoOTA.handle(); 
-
-   if (!mqttClient.connected()) { myCommon.mqtt_reconnect();  }
-   if (mqttClient.connected() ) { mqttClient.loop(); }
-
-   if (millis() - ms_startup > 72000E3) {
-    Serial.println("daily reboot");
-    Serial.println(mqttClient.publish(mqtt_t_heartbeat, "offline"));
-    delay(100);
-    ESP.restart();
-  }
-  
-  
-  if (millis() - ms_sensors_lastread >= 10E3 or ms_sensors_lastread == 0 ){
-    read_sensors();
-  }      
-
-  if (millis() - ms_mqtt_lastsent >= 60E3 or ms_mqtt_lastsent == 0 ){
-      if (WiFi.status() != WL_CONNECTED) { myCommon.connect_wifi(); }
-      if (not isnan(pressure_ICAO) ) {
-        Serial.println(mqttClient.publish(mqtt_t_state_temp, String(baro_temperature).c_str(), true)) ;      
-        Serial.println(mqttClient.publish(mqtt_t_state_baro, String(pressure_ICAO).c_str(), true)) ;      
+  client.loop();
+  if (millis() - ms_mqtt_lastsent >= 60E3 or ms_mqtt_lastsent == 0){
+     read_sensors();
+     
+      if (not isnan(pressure_ICAO) and pressure_ICAO >= 500 and pressure_ICAO <= 2000 ) {
+        Serial.println(client.publish(mqtt_t_temp_state, String(baro_temperature).c_str(), true)) ;      
+        Serial.println(client.publish(mqtt_t_baro_state, String(pressure_ICAO).c_str(), true)) ;      
       }
-      if (not isnan(soil_moisture)  ) { 
-          Serial.println(mqttClient.publish(mqtt_t_state_soil1, String(soil_moisture).c_str(), true)) ;      
+      if (not isnan(soil_perc)  ) { 
+          Serial.println(client.publish(mqtt_t_soil1_state, String(soil_perc).c_str(), true)) ;      
        }
       ms_mqtt_lastsent = millis();
   }
-
- delay(100);
- if (deep_sleep_ms > 0) { Serial.print("sleeping for "); Serial.println(deep_sleep_ms/1000000.00); ESP.deepSleep(deep_sleep_ms); }
-}
-
-
-void handleRoot() {
-  String a = ""; //"<html>" 
-  a = a + myCommon.getHandleRootHeader(__FILE__, /* html */ false);
   
-  
-
-  a = a + "\n\nair pressure\n-------------\n";
-  read_sensors();
-  a = a + "baro temp           : ";
-  a = a + baro_temperature;
-  a = a + " C\npressure raw        : ";
-  a = a + pressure_raw / 100.0;
-  a = a + "\n\nindoors (baro_temperature)  : ";
-  a = a + ( pressure_raw  / pow(1.0 - 0.0065 * LOCALALTUD / (baro_temperature + 273.15), 5.255) / 100.0); // ICAO formula
-
-  a = a + "\noutdoors (ICAO 15C) : ";
-  a = a + pressure_ICAO;
-  a = a + "\noutdoors (BOSCH)    : ";
-  a = a + pressure_bosch;
-  a = a + "\noutdoors (babinet)  : ";
-  a = a + pressure_babinet;
-  a = a + "\n\nanalog value (soil) : ";
-  analog_value = analogRead(A0);
-      
-  a = a + analog_value;
-  a = a + "   (  ";
-  a = a + soil_moisture;
-  a = a + "% )";
-  a = a + "  min=";
-  a = a + analog_value_min;
-  a = a + "  max=";
-  a = a + analog_value_max;
-
-  server.send(200, "text/plain", a);   // Send HTTP status 200 (Ok) and send some text to the browser/client
 }
