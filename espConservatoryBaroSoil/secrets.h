cca68de1811cd99e1ad311c1c4a59921ac720852
Andrew Morty <mortyone@hotmail.com>
2021-08-05 12:13:47 +0100

    Create secrets.h

---
+++ espConservatoryBaroSoil/secrets.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID "XXX"
+#define WIFI_PASS "XXX"
+
+#define MQTT_SERVER "192.168.1.15"
+#define MQTT_PORT   "1883"
+#define MQTT_USER   "mqtt_user"
+#define MQTT_PASS   "mqtt_password"
+
+
+
