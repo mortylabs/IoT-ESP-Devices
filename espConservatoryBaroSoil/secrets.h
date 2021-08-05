7e0e0187bf983261fb97ed78b5dd9fa2fda6285e
Andrew Morty <mortyone@hotmail.com>
2021-08-05 13:08:31 +0100

    Update secrets.h

--- espConservatoryBaroSoil/secrets.h
+++ espConservatoryBaroSoil/secrets.h
@@ -2,8 +2,8 @@
 #define WIFI_PASS "XXX"
 
 #define MQTT_SERVER "192.168.1.15"
-#define MQTT_PORT   "1883"
-#define MQTT_USER   "mqtt_user"
+#define MQTT_PORT   "31883"
+#define MQTT_USER   "mqtt_superdude"
 #define MQTT_PASS   "mqtt_password"
 
 
